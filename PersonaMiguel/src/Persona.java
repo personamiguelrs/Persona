593f891b47aa29fd0841dc515e73e60c22e0f757
Jose <Jose@192.168.0.162>
2019-05-24 23:44:14 +0200

    Editado con el Javadoc

--- PersonaMiguel/src/Persona.java
+++ PersonaMiguel/src/Persona.java
@@ -1,32 +1,71 @@
-
+/**
+ * 
+ * @author Jose
+ * @version 24/05/19
+ * 
+ * Esta es la clase <h5>Persona</h5> que nos dará un nombre, teléfono y dni.
+ */
 public class Persona {
 	String nombre;
 	String dni;
 	String telefono;
-	
-	
-	
+		
+	/**
+	 * Es un constructor en el que se establece los atributos de la persona.
+	 * @param nombre .Establece el nombre.
+	 * @param dni .Establece el dni.
+	 * @param telefono .Establece el teléfono.
+	 */
 	public Persona(String nombre, String dni, String telefono) {
 		super();
 		this.nombre = nombre;
 		this.dni = dni;
 		this.telefono = telefono;
 	}
+	
+	/**
+	 * 
+	 * @return .Devuelve un String con el nombre.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * 
+	 * @param nombre .Usa el parámetro nombre.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * 
+	 * @return .Devuelve un String con el dni.
+	 */
 	public String getDni() {
 		return dni;
 	}
+	
+	/**
+	 * 
+	 * @param dni .Usa el parámetro dni.
+	 */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+	
+	/**
+	 * 
+	 * @return .Devuelve un String con el teléfono.
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
+	
+	/**
+	 * 
+	 * @param telefono .Usa el parámetro teléfono.
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
